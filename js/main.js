809d2e9ff06c7525cadbf7514b9f39de3f60a8bf
Dian <dian.mike.li@gmail.com>
2014-04-13 10:29:27 -0400

    The plan reaches the end...Yet another idea

--- js/main.js
+++ js/main.js
@@ -4,47 +4,29 @@ function Grid(size, position){
 	this.cells = [];
 	this.x = position.x;
 	this.y = position.y;
+	this.currx = (this.x + 1) * 146.25;
+	this.curry = (this.y + 1) * 146.25;
 	//methods
 	this.generateCells;
 }
 
 Grid.prototype.generateCells = function (){
-	var firstrow = $("<div></div>").addClass("flat-grid-row").attr("id", "flat-grid-first-row");
-	var secondrow = $("<div></div>").addClass("flat-grid-row").attr("id", "flat-grid-second-row");
-	var thirdrow = $("<div></div>").addClass("flat-grid-row").attr("id", "flat-grid-third-row");
 	for (i = 0; i < 3; i++){
 		for (j = 0; j < 3; j++){
-			var cell = $("<div></div>").addClass("flat-grid-cell").attr("id", j + "-" + i + "-cell");
 			for (k = 0; k < 3; k++){
-				var row = $("<div></div>").addClass("flat-inner-grid-row");
 				for (l = 0; l < 3; l++){
-					var innerCell = $("<div></div>").addClass("flat-inner-grid-cell");
 					var position = {x : l + j * 3, y : k + i * 3};
 					size = 41.75;
-					if (cell[position.x]) {
-						cell[position.x][position.y] = new Cell(size, position);
+					if (this.cells[position.x]) {
+						this.cells[position.x][position.y] = new Cell(size, position);
+					}else{
+						this.cells[position.x] = [];
+						this.cells[position.x][position.y] = new Cell(size, position);
 					}
-					innerCell.attr("id", position.x + "-" + position.y + "-innerCell");
-					row.append(innerCell);
 				}
-				cell.append(row);
-			}
-			switch (i) {
-				case 0:
-					firstrow.append(cell);
-					break;
-				case 1:
-					secondrow.append(cell);
-					break;
-				case 2:
-					thirdrow.append(cell);
-					break;
 			}
 		}
 	}
-	$("#flat-grid-container").append(firstrow);
-	$("#flat-grid-container").append(secondrow);
-	$("#flat-grid-container").append(thirdrow);
 }
 
 
@@ -53,20 +35,45 @@ function Cell(size, position, div){
 	this.size = size;
 	this.x = position.x;
 	this.y = position.y;
+	this.color;
 	//methods
 	this.update;
 	this.getDiv;
+	this.enlarge;
 }
 
 Cell.prototype.update = function (){
-	div = this.getDiv();
-	div.css("width", this.size);
-	div.css("hight", this.size);
+
+	var cellid = "#" + "innerCell-"+ this.x + "-" + this.y;
+	div = this.getDiv(cellid);
+	var browid = "#" + div.parent().parent().parent().attr("id");
+	var bcellid = "#" + div.parent().parent().attr("id");
+	var rowid = "." + div.parent().attr("class");
+	
+	move(rowid)
+		.set("height", "146.25px")
+		.end();
+	move(bcellid)
+		.set("height", "472px")
+		.set("width", "472px")
+		.end()
+	move(browid)
+		.set("height", "472px")
+		.set("float", "left")
+		.end();
+	move(cellid)
+		.set("height", this.size)
+		.set("width", this.size)
+		.end();
 }
 
-Cell.prototype.getDiv = function (){
-	var id = "#" + this.x + "-" + this.y + "-innerCell";
+Cell.prototype.getDiv = function (id){
 	return $(id);
+}
+
+Cell.prototype.enlarge = function (){
+	this.size = 146.25;
+	this.update();
 }
 
 function MouseManager(){
@@ -76,7 +83,7 @@ function MouseManager(){
 	this.on;
 	this.off;
 	this.respond;
-	this.listen();
+	this.listen;
 }
 
 MouseManager.prototype.on = function (event, callback) {
@@ -103,43 +110,169 @@ MouseManager.prototype.respond = function (event, data){
 
 MouseManager.prototype.listen = function (data){
 	var self = this;
-	$(".flat-grid-cell").hover(function (){
-		self.respond("bigcell-hover", {div : $(this)});
+	$(".indicator-cell").hover(function (){
+		self.respond("indicator-hover", {div:$(this)});
 	});
 
-	$(".flat-inner-grid-cell").hover(function (){
-		self.respond("smallcell-hover", {div : $(this)});
+	$(".tictactoe-cell").hover(function (){
+		self.respond("tictactoe-hover", {div:$(this), grids: data.grids});
 	});
 
-	$(".flat-grid-cell").click(function (){
-		self.respond("bigcell-click", {div : $(this), });
+	$(".indicator-cell").click(function (){
+		self.respond("indicator-click", {div:$(this), grids: data.grids});
 	});
 
-	$(".flat-inner-grid-cell").click(function (){
-		self.respond("smallcell-click", {div : $(this)});
+	$(".tictactoe-cell").click(function (){
+		self.respond("tictactoe-click", {div:$(this), grids: data.grids});
 	});
 };
 
-function GameManager(size, players){
+function HTMLActuator(){
+	this.indicatorSelector = ".indicator-board";
+	this.tictactoeSelector = "tictactoe-board";
+	this.gameContainerSelector = ".game-container";
+	this.indicatorBoard = "";
+	this.tictactoeBoard = "";
+}
+
+HTMLActuator.prototype.shrinkIndicator = function (x, y) {
+	var self = this;
+	$.each($(self.indicatorSelector).children(), function (index_row, row) {
+		$.each($(row).children(), function (index_cell, cell) {
+			$.each($(cell).children(), function(index_innerRow, innerRow) {
+				$.each($(innerRow).children(), function (index_innerCell, innerCell) {
+					$(innerCell).addClass("indicator-innerCell-shrinked");
+				});
+				$(innerRow).addClass("indicator-innerRow-shrinked");
+			});
+			$(cell).addClass("indicator-cell-shrinked");
+			if(index_row == y && index_cell == x){
+				$(cell).addClass("selected");
+			}
+		});
+		$(row).addClass("indicator-row-shrinked");
+	});
+}
+
+HTMLActuator.prototype.showTictactow = function (grid) {
+	$(this.gameContainerSelector).append(this.tictactoeBoard);
+	var div = $(this.tictactoeSelector);
+	$.each(div.children(), function (index_row, row) {
+		$.each($(row).children(), function (index_cell, cell) {
+			var c = grid.cells[index_cell][index_row];
+			if (c.color){
+				$(cell).css("color", c.color);
+			}
+		});
+	});
+}
+
+HTMLActuator.prototype.init = function (){
+	this.tictactoeBoard = $("<div></div>").addClass("tictactoe-board");
+	this.indicatorBoard = $("<div></div>").addClass("indicator-board");
+	for (y = 0; y < 3; y++){
+		var row = $("<div></div>").addClass("tictactoe-row");
+		for(x = 0; x < 3; x++){
+			var cell = $("<div></div>").addClass("tictactoe-cell").addClass("tictactoe-cell-" + x + "-" + y);
+			row.append(cell);
+		}
+		this.tictactoeBoard.append(row);
+	}
+	for (y1 = 0; y1 < 3; y1++){
+		var row = $("<div></div>").addClass("indicator-row");
+		for (x1 = 0; x1 < 3; x1++){
+			var cell = $("<div></div>").addClass("indicator-cell").addClass("indicator-cell-" + x1 + "-" + y1);
+			for (y2 = 0; y2 < 3; y2++){
+				var innerRow = $("<div></div>").addClass("indicator-innerRow");
+				for (x2 = 0; x2 < 3; x2++){
+					var innerCell = $("<div></div>").addClass("indicator-innerCell");
+					innerCell.addClass("innerCell-" + (x2 + x1 * 3) + "-" + (y2 + y1 * 3));
+					innerRow.append(innerCell);
+				}
+				cell.append(innerRow);
+			}
+			row.append(cell);
+		}
+		this.indicatorBoard.append(row);
+	}
+	
+	$(this.gameContainerSelector).append(this.indicatorBoard);
+}
+
+
+function GameManager(){
 	//attributes
-	this.players = players;
+	this.grids = [[], [], []]
+	this.mouseManager;
+	this.animation;
 	//methods
-	this.zoomIn;
-	this.zoomOut;
 	this.setUp;
 };
 
-function Animation(){
-	this.zoomIn;
-	this.zoomOut;
-}
-
-Animation.prototype.zoomIn = function(grid){
-	var cells = grid.cells;
-	for (i = y; i < 3; i++){
-		for (x = 0; x < 3; x++){
-			cells[x][y].size = 146.25;
-			cells[x][y].update;
+GameManager.prototype.setUp = function () {
+	this.mouseManager = new MouseManager();
+	this.htmlActuator = new HTMLActuator();
+	this.htmlActuator.init();
+	var bgrid = new Grid(0, {x: 1, y : 2});
+	bgrid.generateCells();
+	for (y1 = 0; y1 < 3; y1++){
+		for(x1 = 0; x1 < 3; x1++){
+			var grid = new Grid(146.25, {x:x1, y:y1});
+			for(y2 = 0; y2 < 3; y2++){
+				for(x2 = 0; x2 < 3; x2++){
+					if(grid.cells[x2]){
+						grid.cells[x2][y2] = bgrid.cells[x1 * 3 + x2][y1 * 3 + y2];
+					}else{
+						grid.cells[x2] = [];
+						grid.cells[x2][y2] = bgrid.cells[x1 * 3 + x2][y1 * 3 + y2];
+					}	
+				}
+			}
+			this.grids[x1][y1] = grid;
 		}
 	}
+	self = this;
+	this.mouseManager.on("indicator-hover", function(data){
+		var div = data.div;
+		div.css("opacity", "0.5");
+		div.mouseleave(function(){
+			div.css("opacity", "1")
+		});
+	});
+	this.mouseManager.on("indicator-click", function(data){
+		var div = data.div;
+		var grids1 = data.grids;
+		pos = div.attr("class").split(" ")[1].split("-");
+		x = parseInt(pos[2]);
+		y = parseInt(pos[3]);
+		self.htmlActuator.shrinkIndicator(x, y);
+		self.htmlActuator.showTictactow(grids1[x][y]);
+		self.mouseManager.off("indicator-hover");
+		self.mouseManager.on("tictactoe-hover", function(data) {
+			var div = data.div;
+			div.css("opacity", "0.5");
+			div.mouseleave(function(){
+				div.css("opacity", "1")
+			});
+		});
+		self.mouseManager.on("tictactoe-click", function (data) {
+			var div = data.div;
+			var grid = data.grids[x][y];
+			pos = div.attr("class").split(" ")[1].split("-");
+			x = pos[2];
+			y = pos[3];
+			console.log(div);
+			if (self.color = "red"){
+				div.addClass("red")
+				grid.cells[x][y].color = "red";
+			}else{
+				div.addClass("blue");
+				grid.cells[x][y].color = "blue";
+			}
+		});
+		self.mouseManager.listen({grids: data.grids});
+	});
+	this.mouseManager.listen({grids: this.grids});
+	
 }
+
